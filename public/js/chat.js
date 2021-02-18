3a803a841a4439c23c70f7c20cac1c3aa72051bd
astatsuya <sharekobekko@gmail.com>
2021-02-18 16:19:23 +0900

    feature: send message

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,15 @@
 const socket = io();
 
-socket.on("countUpdated", (count) => {
-  console.log("The count has been updated!", count);
+socket.on("Welcome!", () => {
+  console.log("Welcome!");
 });
 
-document.querySelector("#increment").addEventListener("click", () => {
-  console.log("clicked");
-  socket.emit("increment");
+socket.on("message", (message) => {
+  console.log(message);
+});
+
+document.querySelector("#message-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = e.target.elements.message.value;
+  socket.emit("sendMessage", message);
 });
--- src/index.js
+++ src/index.js
@@ -12,16 +12,14 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-let count = 0;
-
 io.on("connection", (socket) => {
   console.log("New WebSocket connection");
 
-  socket.emit("countUpdated", count);
+  socket.emit("Welcome!");
 
-  socket.on("increment", () => {
-    count++;
-    io.emit("countUpdated", count);
+  socket.on("sendMessage", (message) => {
+    console.log(message);
+    io.emit("message", message);
   });
 });
 
